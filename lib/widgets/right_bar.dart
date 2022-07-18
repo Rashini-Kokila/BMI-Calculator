5f3e3614dddf6e092d442d6059dddf33d9221662
Rashini-Kokila <83535591+Rashini-Kokila@users.noreply.github.com>
2022-07-18 12:56:09 +0530

    Add files via upload

---
+++ lib/widgets/right_bar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants/app_constants.dart';
+
+class RightBar extends StatelessWidget {
+  final double barWidth;
+
+  const RightBar({Key key, @required this.barWidth = 0.0}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: 25,
+          width: barWidth,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+              color: accentHexColor),
+        ),
+      ],
+    );
+  }
+}
